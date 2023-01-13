3d3294a29a3772d5b1be38dbc66bfc8e6f299f81
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2023-01-13 11:29:47 +0100

    nx reactspace & libs

---
+++ apps/reactspace/src/app/core/layouts/Header.tsx
@@ -0,0 +1,32 @@
+import { AppBar, Toolbar, Theme, Box } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+export function Header() {
+  const classes = useStyles();
+
+  return (
+    <AppBar
+      className={classes.header}
+      position="static"
+      color="default"
+      elevation={0}
+    >
+      <Toolbar className={classes.toolbar}>
+        <Box
+          component="img"
+          src="/assets/logo.jpg"
+          sx={{ width: 175, height: 40 }}
+        />
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  header: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+}));
